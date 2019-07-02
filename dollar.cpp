81b85630c1a98d9c4dfc8a37685c383d850005d9
Orayaa <52215224+Orayaa@users.noreply.github.com>
2019-07-02 23:22:26 +0700

    Add files via upload

---
+++ dollar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float Number;
+	cout << "Enter Bath : ";
+	cin >> Number;
+	float Result (0);
+	float Result1 (0);
+	Result = Number * 0.033;
+	Result1 = Number * 0.029;
+	cout << Result << " Dollar." << "\n";
+	cout << Result1 << " Euro." << "\n";
+
+	return(0);
+
+
+
+
+}
